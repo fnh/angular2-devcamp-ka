88fa6a45de6581b3bc3acd34feedfb7e97861c87
Fabian Holzer <fabian.holzer.lists@gmail.com>
2016-09-23 23:06:54 +0200

    Revert "Step 6: Get todos via http service call"
    This reverts commit 0bf6e6be23a8cc324ca158ba9d2ba27080fabbc5.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from "./app.component";
 import { TodoComponent } from "./todo.component";
 import { TaskComponent } from "./task.component";
 
-import { TodoService } from "./todo.service";
-
 @NgModule({
     imports: [
         BrowserModule,
         HttpModule
     ],
     declarations: [AppComponent, TodoComponent, TaskComponent],
-    providers: [TodoService],
+    providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
